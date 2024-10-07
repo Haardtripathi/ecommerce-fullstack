ef62de53173b0f440bdd714fc26f01d29d72e30c
unknown <haard.tripathi14@gmail.com>
2024-10-07 18:48:58 +0530

    changes made again 2

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    FRONTEND_URL: process.env.FRONTEND_URL || 'https://ecomm-fullstack-demo1.netlify.app/',
+    FRONTEND_URL: process.env.FRONTEND_URL || 'https://ecomm-fullstack-demo1.netlify.app',
     BACKEND_URL: process.env.BACKEND_URL || 'https://ecommerce-fullstack-1-dsid.onrender.com',
     NODE_ENV: process.env.NODE_ENV || 'development'
 };
